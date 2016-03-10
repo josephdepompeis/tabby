f642c4fc9872212a48546ecb06f2f8bb841f8c6e
Joseph DePompeis <josephdepompeis@gmail.com>
2016-03-10 10:04:38 -0500

    another failed attempt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,17 @@
 class HomeController < ApplicationController
 
+  def new
+  end
+
+  def create
+      @survey = Survey.create!(survey_params)
+end
+
+
   def form
     @survey = Survey.new
     if request.post?
-      @survey = Survey.create!(survey_params)
-    end
+      redirect_to home_create
   end
 
   private def survey_params
